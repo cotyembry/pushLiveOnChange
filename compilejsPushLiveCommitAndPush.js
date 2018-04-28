96455499799158ae8d0b1ef118576fbceea75918
Coty Embry <cotyembry@gmail.com>
2018-04-28 12:42:20 -0500

    first commit ^_^

---
+++ compilejsPushLiveCommitAndPush.js
@@ -0,0 +1,14 @@
+// this file will attempt to:
+//     1-execute command to start a process that will:
+//         1.1-listen to a file and when it is saved called "commitMessageAndPushOnChange.txt"
+//         1.2-run a webpack command to properly compile the app to
+//         1.3-make a commit message with the first line in the commitMessageAndPushOnChange.txt file
+//         1.4-execute a git command to push to github
+//         1.5-push to surge
+
+
+
+
+
+
+
